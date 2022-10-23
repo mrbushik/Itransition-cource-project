317bd97451d52a0d071fcc7795952da5b0a9fca0
Nikita <mrbushik1@gmail.com>
2022-10-23 20:22:36 +0300

    delete never used info and files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import UserPage from './Components/pages/userPage';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import CollectionPosts from './Components/pages/collectionPosts';
 import Login from './Components/pages/login';
@@ -10,13 +9,6 @@ import UsersCollections from './Components/pages/usersCollections';
 import AdminCollections from './Components/pages/adminCollections';
 
 function App() {
-  const theme = useSelector(({ theme }) => theme.theme);
-
-  if (theme === 'dark') {
-    require('../src/Components/theme/dark.css');
-  } else if (theme === 'light') {
-    require('../src/Components/theme/light.css');
-  }
   return (
     <>
       <Switch>
@@ -24,11 +16,8 @@ function App() {
         <Route path="/admin-collections/:Id" component={CollectionPosts} />
         <Route path="/admin-collections" component={AdminCollections} />
         <Route path="/login" component={Login} />
-
         <Route path="/collection/:Id" component={CollectionPosts} />
         <Route path="/collection" component={UserPage} />
-        {/* <Route path="/" component={MainPage} />
-         */}
         <Route path="/:Id" component={UsersCollections} />
         <Route path="/" component={MainPage} />
         <Redirect to="/" />
